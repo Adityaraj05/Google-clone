5b5ea71798c6424c3730a2b86a502b67df89d10b
Aditya raj <adityarajavi05@gmail.com>
2023-05-08 00:19:17 +0530

    span tag insited of p

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,21 +6,21 @@ export default function Footer() {
     <footer className="absolute bottom-10 text-sm text-gray-500 bg-[#f2f2f2] w-full">
     
       <div className=" px-8 py-3">
-       <p className="link"><CountryLookup /></p> 
+       <span className="link"><CountryLookup /></span> 
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense'>
         <div className='flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2'>
-           <GlobeIcon className='h-5 mr-1 text-green-700' /> <p className='link'>Carbon netural since 2007</p>
+           <GlobeIcon className='h-5 mr-1 text-green-700' /> <span className='link'>Carbon netural since 2007</span>
         </div>
         <div className='flex justify-center space-x-8 whitespace-nowrap md:justify-self-start'>
-            <p className="link">Advertising</p>
-            <p className="link">Business</p>
-            <p className="link">How Search works</p>
+            <span className="link">Advertising</span>
+            <span className="link">Business</span>
+            <span className="link">How Search works</span>
         </div>
         <div className='flex justify-center space-x-8 md:ml-auto '>
-        <p className="link">Privacy</p>
-        <p className="link">Terms</p>
-        <p className="link">Settings</p>
+        <span className="link">Privacy</span>
+        <span className="link">Terms</span>
+        <span className="link">Settings</span>
        </div>
     </div>
       
